f493669e54d5b27208496a5b0cde86808467ae50
Jason Tung <jasontung123@yahoo.com>
2019-02-02 03:53:23 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,6 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  rand("1", "2", "3", "4", "5", "6")# code goes here
+  rand("1", "2", "3", "4", "5", "6")
+  roll# code goes here
 end
